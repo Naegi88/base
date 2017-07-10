3845b7e9baf8f8e85570ede482b4afc8bb785924
Naegi88 <sili.naegi@gmx.ch>
2017-07-10 15:40:14 +0200

    processed main.c and create makefile

--- main.c
+++ main.c
@@ -1,9 +1,19 @@
 #include <stdlib.h>
 #include <avr/io.h>
 
+
+#define LED1_Aus PORTD &= ~(1<<PD1);
+#define LED1_Ein PORTD |= (1<<PD1);
+
 int main(void)
 {
+DDRD = 0xFF;//Port D als Ausgag setzen
 
+	PORTD = 0xFF;
+	
+	LED1_Aus;//LED 1 Ausschalten
+	
+	
 	while(1)
 	{
 
