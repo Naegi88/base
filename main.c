063a6d3e6e8dcd25fc23cb273caf4890109b5568
Naegi88 <sili.naegi@gmx.ch>
2017-07-11 13:50:52 +0200

    ab Jetz anhaltbar

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 #include <avr/io.h>
 #include <until/delay.h>
 
+#define Taster1_1 !(PINB & (1<<PB1))
+#define Taster1_0 (PINB & (1<<PB1))
+
+#define Taster2_1 !(PINB & (1<<PB2))
+#define Taster2_0 (PINB & (1<<PB2))
+
+
 #define LED1_0 PORTD &= ~(1<<PD0);
 #define LED1_1 PORTD |= (1<<PD0);
 
@@ -35,72 +42,126 @@ int main(void)
 
 	DDRD = 0xFF;//Port D als Ausgag setzen
 	
+	DDRB &= ~(1<<PB1);//Port B als eingang setzen
+	
+	PORTB |= (1<<PB1);//Pull up wiederstand aktivieren 
+	PORTB |= (1<<PB2);
+	
 		while(1)
 	{
 
-	for(x=0; x<50000; x++) //Schleiff erstellen 
+	while(Taster1_1) // Bei Tasterdrücken LEDs stoppen 
 	{
 		LED1_1 LED8_1;
-	}
+	}	
 	
-		LED1_0;
-		LED8_0;			   //LEDs abschalten
+			for(x=0; x<30000; x++) //Schleiff erstellen 
+			{
+				LED1_1;
+				LED8_1;
+			}
+		
+				LED1_0;
+				LED8_0;//LEDs abschalten
 	
-	for(x=0; x<50000; x++)
+	
+	while(Taster1_1)
 	{
 		LED2_1 LED7_1;
-	}
-	
-		LED2_0;
-		LED7_0;
-	
-	for(x=0; x<50000; x++)
+	}	
+		
+			for(x=0; x<30000; x++)
+			{
+				LED2_1;
+				LED7_1;
+			}
+		
+				LED2_0;
+				LED7_0;
+		
+	while(Taster1_1)
 	{
 		LED3_1 LED6_1;
 	}
 	
-		LED3_0;
-		LED6_0;
-
-	for(x=0; x<50000; x++)
+			for(x=0; x<30000; x++)
+			{
+				LED3_1;
+				LED6_1;
+			}
+			
+				LED3_0;
+				LED6_0;
+				
+	while(Taster1_1)
 	{
 		LED4_1 LED5_1;
-	}
-	
-		LED4_0;
-		LED5_0;
-	
-	for(x=0; x<50000; x++)
+	}	
+
+			for(x=0; x<30000; x++)
+			{
+				LED4_1; 
+				LED5_1;
+			}
+			
+				LED4_0;
+				LED5_0;
+				
+	while(Taster1_1)
 	{
 		LED5_1 LED4_1;
 	}
 	
-		LED5_0;
-		LED4_0;
-	
-	for(x=0; x<50000; x++)
+			for(x=0; x<30000; x++)
+			{
+				LED5_1;
+				LED4_1;
+			}
+			
+				LED5_0;
+				LED4_0;
+				
+	while(Taster1_1)
 	{
 		LED6_1 LED3_1;
-	}
+	}			
 	
-		LED6_0;
-		LED3_0;
-	
-	for(x=0; x<50000; x++)
+			for(x=0; x<30000; x++)
+			{
+				LED6_1;
+				LED3_1;
+			}
+			
+				LED6_0;
+				LED3_0;
+				
+	while(Taster1_1)
 	{
 		LED7_1 LED2_1;
-	}
+	} 
+				
+			
+			for(x=0; x<30000; x++)
+			{
+				LED7_1;
+				LED2_1;
+			}
+			
+				LED7_0;
+				LED2_0;
 	
-		LED7_0;
-		LED2_0;
-	
-	for(x=0; x<50000; x++)
+	while(Taster1_1)
 	{
 		LED8_1 LED1_1;
-	}
-	
-		LED8_0;
-		LED1_0;
+	}	
+			for(x=0; x<30000; x++)
+			{
+				LED8_1;
+				LED1_1;
+			}
+		
+				LED8_0;
+				LED1_0;
 	 
 
 	}//end of while
