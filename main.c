96feead6c38210a6a3b04cd8f4eccdfc5c1af448
Naegi88 <sili.naegi@gmx.ch>
2017-07-12 13:56:47 +0200

    7- Segment zähler

--- main.c
+++ main.c
@@ -2,301 +2,331 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define F_CPU 8000000UL					//Zeit definiert
+#define F_CPU 80000000UL
 
-#define TASTE1 !(PINB & (1<<PB1))		//Taster1
-#define TASTE2 !(PINB & (1<<PB2))		//Taster2
-#define TASTE3 !(PINB & (1<<PB3))		//Taster3
+#define Taster1_1 !(PINB & (1<<PB1))
+#define Taster1_0 (PINB & (1<<PB1))
 
-#define LED0_AUS PORTD &= ~(1<<PD0);	//löscht bit PD0
-#define LED0_EIN PORTD |= (1<<PD0);		//setzt PD0
+#define Taster2_1 !(PINB & (1<<PB2))
+#define Taster2_0 (PINB & (1<<PB2))	//Pin abfrage
 
-#define LED1_AUS PORTD &= ~(1<<PD1);	//löscht bit PD1
-#define LED1_EIN PORTD |= (1<<PD1);		//setzt PD1
+#define Taster3_1 !(PINB & (1<<PB3))
+#define Taster3_0 (PINB & (1<<PB3))
 
-#define LED2_AUS PORTD &= ~(1<<PD2);	//löscht bit PD2
-#define LED2_EIN PORTD |= (1<<PD2);		//setzt PD2
 
-#define LED3_AUS PORTD &= ~(1<<PD3);	//löscht bit PD3
-#define LED3_EIN PORTD |= (1<<PD3);		//setzt PD3
+#define LED1_0 PORTD &= ~(1<<PD0);
+#define LED1_1 PORTD |= (1<<PD0);
 
-#define LED4_AUS PORTD &= ~(1<<PD4);	//löscht bit PD4
-#define LED4_EIN PORTD |= (1<<PD4);		//setzt PD4
+#define LED2_0 PORTD &= ~(1<<PD1);
+#define LED2_1 PORTD |= (1<<PD1);
 
-#define LED5_AUS PORTD &= ~(1<<PD5);	//löscht bit PD5
-#define LED5_EIN PORTD |= (1<<PD5);		//setzt PD5
+#define LED3_0 PORTD &= ~(1<<PD2);
+#define LED3_1 PORTD |= (1<<PD2);
 
-#define LED6_AUS PORTD &= ~(1<<PD6);	//löscht bit PD6
-#define LED6_EIN PORTD |= (1<<PD6);		//setzt PD6
+#define LED4_0 PORTD &= ~(1<<PD3);
+#define LED4_1 PORTD |= (1<<PD3);
 
-#define LED7_AUS PORTD &= ~(1<<PD7);	//löscht bit PD7
-#define LED7_EIN PORTD |= (1<<PD7);		//setzt PD7
+#define LED5_0 PORTD &= ~(1<<PD4);
+#define LED5_1 PORTD |= (1<<PD4);
+
+#define LED6_0 PORTD &= ~(1<<PD5);
+#define LED6_1 PORTD |= (1<<PD5);
+
+#define LED7_0 PORTD &= ~(1<<PD6);
+#define LED7_1 PORTD |= (1<<PD6);
+
+#define LED8_0 PORTD &= ~(1<<PD7);
+#define LED8_1 PORTD |= (1<<PD7);     //Alle LEDs definiert mit Behfehl 1 und 0 
 
 int main(void)
 {
+	short zeit1;
+	short zeit2;
+	long x;
+	char y;
+	char p;
+	char q;
 	
-	DDRD = 0xFF;						//Port D als Ausgang setzen
-	PORTD = 0x00;						// alle Pins an Port D auf high setzen
+	zeit1 = 50;
+	zeit2 = 100;
+	x = 1;
+	y = 0;
+	p = 0;
+	q = 0;
 	
+	DDRD = 0xFF;//Port D als Ausgag setzen
 	
-	DDRB &= ~(1<<PB1);					//Eingang1
-	PORTB |= (1<<PB1);					//Pull-up Widerstand
-	DDRB &= ~(1<<PB2);					//Eingang2
-	PORTB |= (1<<PB2);					//Pull-up Widerstand
-	DDRB &= ~(1<<PB3);					//Eingang2
-	PORTB |= (1<<PB3);					//Pull-up Widerstand
+	DDRB &= ~(1<<PB1);//Port B als eingang setzen
 	
+	PORTB |= (1<<PB1);//Pull up wiederstand aktivieren 
+	PORTB |= (1<<PB2);
+	PORTB |= (1<<PB3);
 	
+		while(1)
+	{
 	
-	
-	short zeita=200;
-	short zeitb=25;
-	
-	while(1) {
-	
-		if(TASTE2){
-		
-			if(TASTE1){
+			if((Taster1_1) || (y > 0) && (y < 3))
+			{	LED1_0
+				LED2_0
+				LED3_0
+				LED4_0
+				LED5_0
+				LED6_0
+				LED7_0
+				LED1_1
+				
+				_delay_ms(zeit1);
 			
-			LED0_EIN;
+				LED2_1
+				
+				_delay_ms(zeit1);
+				
+				LED3_1
+				
+				_delay_ms(zeit1);
 			
-			_delay_ms(zeita);
+				LED4_1
+				
+				_delay_ms(zeit1);
 			
+				LED5_1
+				
+				_delay_ms(zeit1);
+				
+				LED6_1
+				
+				_delay_ms(zeit1);
+				
+				LED1_0
+				
+				_delay_ms(zeit1);
 			
-			LED0_AUS;
-			LED1_EIN;
+				LED2_0
+				
+				_delay_ms(zeit1);
+				
+				LED3_0
+				
+				_delay_ms(zeit1);
 			
+				LED4_0
+				
+				_delay_ms(zeit1);
 			
-			_delay_ms(zeita);		
-			
-			
-			LED1_AUS;
-			LED2_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED2_AUS;
-			LED3_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED3_AUS;
-			LED4_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED4_AUS;
-			LED5_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED5_AUS;
-			LED6_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED6_AUS;
-			LED7_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			LED7_AUS;
+				LED5_0
+				
+				_delay_ms(zeit1);
+				
+				LED6_0
+				
+				_delay_ms(zeit1);
+			y++;
+			p = 0;
 			
 			}
 			
-			else{
+			
+			
+			else if (p == 0)	// 0
+			{
+				y = 0;
+				LED1_1
+				LED2_1
+				LED3_1
+				LED4_1
+				LED5_1
+				LED6_1
+				
 
-			LED0_EIN;
-			
-			_delay_ms(zeitb);
-			
-			
-			LED0_AUS;
-			LED1_EIN;
-			
-			
-			_delay_ms(zeitb);		
-			
-			
-			LED1_AUS;
-			LED2_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED2_AUS;
-			LED3_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED3_AUS;
-			LED4_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED4_AUS;
-			LED5_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED5_AUS;
-			LED6_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED6_AUS;
-			LED7_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			LED7_AUS;
-			
-			}
-		}
-		
-		else{
-			
-			if(TASTE1){
-			
-			LED7_EIN;
-			
-			_delay_ms(zeita);
-			
-			
-			LED7_AUS;
-			LED6_EIN;
-			
-			
-			_delay_ms(zeita);		
-			
-			
-			LED6_AUS;
-			LED5_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED5_AUS;
-			LED4_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED4_AUS;
-			LED3_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED3_AUS;
-			LED2_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED2_AUS;
-			LED1_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			
-			LED1_AUS;
-			LED0_EIN;
-			
-			
-			_delay_ms(zeita);
-			
-			LED0_AUS;
-			
 			}
 			
-			else{
+			
+			if(Taster2_1)
+				{
+					p++;
+					_delay_ms(170);
+				}
+				
+			switch(p)
+			{
+				case 1: 	LED1_0
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_0
+							LED7_0
+							break;
+							
+				case 2: 	LED1_1
+							LED2_1
+							LED3_0
+							LED4_1
+							LED5_1
+							LED6_0
+							LED7_1
+							break;	
+						
+				case 3: 	LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_0
+							LED7_1
+							break;
+								
+				case 4: 	LED1_0
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_1
+							LED7_1
+							break;
+							
+				case 5: 	LED1_1
+							LED2_0
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_1
+							LED7_1
+							break;
+							
+				case 6: 	LED1_1
+							LED2_0
+							LED3_1
+							LED4_1
+							LED5_1
+							LED6_1
+							LED7_1
+							break;
+							
+				case 7: 	LED1_1
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_0
+							LED7_0
+							break;
+								
+				case 8: 	LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_1
+							LED6_1
+							LED7_1
+							break;	
+							
+				case 9: 	LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_1
+							LED7_1
+							break;
+							
+				case 10:	LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_1
+							LED7_1
+							_delay_ms(zeit2);// 9
+							
+							LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_1
+							LED6_1
+							LED7_1
+							_delay_ms(zeit2);// 8
+							
+							LED1_1
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_0
+							LED7_0
+							_delay_ms(zeit2);// 7
+							
+							LED1_1
+							LED2_0
+							LED3_1
+							LED4_1
+							LED5_1
+							LED6_1
+							LED7_1
+							_delay_ms(zeit2);// 6
+							
+							LED1_1
+							LED2_0
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_1
+							LED7_1
+							_delay_ms(zeit2);// 5
+							
+							LED1_0
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_1
+							LED7_1
+							_delay_ms(zeit2);// 4
+							
+							LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_0
+							LED6_0
+							LED7_1
+							_delay_ms(zeit2);// 3
+							
+							LED1_1
+							LED2_1
+							LED3_0
+							LED4_1
+							LED5_1
+							LED6_0
+							LED7_1
+							_delay_ms(zeit2);// 2
+							
+							LED1_0
+							LED2_1
+							LED3_1
+							LED4_0
+							LED5_0
+							LED6_0
+							LED7_0
+							_delay_ms(zeit2);// 1
+							
+							LED1_1
+							LED2_1
+							LED3_1
+							LED4_1
+							LED5_1
+							LED6_1
+							LED7_0
+							_delay_ms(zeit2);// 0
+							
+							p = 0;
+							
+							break;
+							
+			}
+			
 
-			LED7_EIN;
-			
-			_delay_ms(zeitb);
-			
-			
-			LED7_AUS;
-			LED6_EIN;
-			
-			
-			_delay_ms(zeitb);		
-			
-			
-			LED6_AUS;
-			LED5_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED5_AUS;
-			LED4_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED4_AUS;
-			LED3_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED3_AUS;
-			LED2_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED2_AUS;
-			LED1_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			
-			LED1_AUS;
-			LED0_EIN;
-			
-			
-			_delay_ms(zeitb);
-			
-			LED0_AUS;
-			
-			}
-			
-		}
-		
 	}//end of while
 	
 	return 0;
