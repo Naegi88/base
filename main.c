1c79e28060b78a455e584023440e11663ec7405e
Naegi88 <sili.naegi@gmx.ch>
2017-08-17 10:45:52 +0200

    ungenaue stoppuhr

--- main.c
+++ main.c
@@ -4,11 +4,15 @@
 #include <avr/interrupt.h>
 
 
-#define F_CPU 80000000UL
+
+#define F_CPU 8000000UL
 
 #define Aktivierungstaster !(PINC & (1<<PC0))
-#define Codeschalter !(PINC & (1<<PC1))
-#define Tuerkontakt !(PINC & (1<<PC2))
+#define Tuerkontakt !(PINC & (1<<PC1))
+#define taster0 !(PINC & (1<<PC5))
+#define taster1 !(PINC & (1<<PC2))
+#define taster2 !(PINC & (1<<PC3))
+#define taster3 !(PINC & (1<<PC4))
 
 #define led1_1 (PORTB |= (1<<PB4))
 #define led1_0 (PORTB &= ~(1<<PB4))
@@ -23,14 +27,39 @@
 
 long o = 0;
 long p = 0;
+char min = 2;
 char state = AUS;
+int ms_100 = 0;
+long sec = 1;
+uint16_t ISR_zaeler;
 
+ISR (TIMER0_OVF_vect)
+{
+	TCNT0 = 0;
+	ISR_zaeler++;
+	if(ISR_zaeler== 13)
+	{
+		ISR_zaeler=0;
+		ms_100++;
+	}
+	
+	if(ms_100==10)
+	{
+		ms_100 = 0;
+		sec++;
+	}
+	
+}
 
 
 
 int main(void)
 {
-
+	TCCR0A		= 0x00;
+	TCCR0B		= 0x04;
+	TIMSK0		|= (1<<TOIE0);
+	TIFR0		|(1<<TOV0);
+	sei();
 	
 	DDRD = 0xFF;
 	DDRB = 0xFF;//Ausgängesetzen
@@ -41,112 +70,22 @@ int main(void)
 	
 	PORTC |= (1<<PD0);
 	PORTC |= (1<<PD1);
-	PORTC |= (1<<PD2);//Pull Up Widerstand
+	PORTC |= (1<<PD2);
+	PORTC |= (1<<PD3);
+	PORTC |= (1<<PD4);
+	PORTC |= (1<<PD5);//Pull Up Widerstand
 	
 	
 	lcd_init(LCD_DISP_ON_CURSOR_BLINK);  // initialisieren		
 	lcd_clrscr();
 	
 	
-	lcd_gotoxy(3,1);
-	lcd_puts("Ausgeschaltet");
 
 	while(1)
 	{
-	
-	
-		
-	switch(state)
-	{
-		case AUS:			if(Aktivierungstaster)
-								{
-									state = SCHARF;
-									
-									lcd_clrscr();
-								lcd_gotoxy(6,0);
-								lcd_puts("AKTIV!");
-								lcd_gotoxy(8,1);
-								lcd_puts("und");
-								lcd_gotoxy(6,2);
-								lcd_puts("SCHARF");
-								}
-								
-								beeper1_0;
-								led1_0;
-								led2_1;
-			break;
-			
-		case SCHARF:		if(Codeschalter)
-								{
-									state = AUS;
-									
-									lcd_clrscr();
-								lcd_gotoxy(3,1);
-								lcd_puts("Ausgeschaltet");
-								}
-								
-							if(Tuerkontakt)
-								{
-									state = ALARM;
-									
-									lcd_clrscr();
-								lcd_gotoxy(6,1);
-								lcd_puts("ALARM!");
-								
-								}
-								
-								beeper1_0;
-								led2_0;
-								o++;
-								if(o>120000)
-								{
-									led1_1;
-								}
-								
-								if(o==129000)
-								{
-									led1_0;
-									o = 0;
-								}
-			break;			
-			
-		case ALARM:				if(Codeschalter)
-								{
-									state = AUS;
-									
-								lcd_clrscr();
-								lcd_gotoxy(3,1);
-								lcd_puts("Ausgeschaltet");
-								beeper1_0;
-								
-								}
-								
-								
-								
-								if(p<30000)
-								{
-									led2_0;
-									led1_1;
-									beeper1_1;
-									p++;
-								}
-								
-								if(p>29999)
-								{
-									led1_0;
-									led2_1;
-									beeper1_0;
-									p++;
-								}
-								
-								if(p==60000)
-								{
-									p=0;
-								}
-			break;
-	}
+	lcd_gotoxy(10,1);
+	lcd_count_16(sec);
 	}
 	return 0;
 }//end of main
-
 
